bf5bd5252f3fa200884c75bbf4a6921b8c8065b0
Kevin Mitchell <>
2023-06-22 09:51:36 -0400

    Initial test setup with CDK and Next.js
    This is the setup I used in https://github.com/jetbridge/cdk-nextjs/issues/115 **however note that I removed my account number from the CDK stack** - it should be clear where a person would enter their account number or region.

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { CdkStack } from '../lib/cdk-stack';
+import * as cdk from "aws-cdk-lib";
+import { CdkStack } from "../lib/cdk-stack";
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack');
+new CdkStack(app, "CdkStack", {
+  env: { account: "ENTER YOUR ACCOUNT NUMBER HERE", region: "us-east-2" },
+});
--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,19 +1,13 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Nextjs } from "cdk-nextjs-standalone";
+import { Construct } from "constructs";
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    new Nextjs(this, "Web", {
+      nextjsPath: "./web", // relative path to nextjs project root
     });
-
-    const topic = new sns.Topic(this, 'CdkTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
